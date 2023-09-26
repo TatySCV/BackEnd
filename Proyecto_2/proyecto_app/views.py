8f02c68c5b4eb61d9bd827ecb5412662c746d8c3
TatySCV <tatiana.campos03@inacapmail.cl>
2023-09-26 12:02:45 -0300

    Clase26

--- Proyecto_2/proyecto_app/views.py
+++ Proyecto_2/proyecto_app/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'view/index.html')
 def electronica(request):
-    data = {"titulo" : "Electrónica"}
+    data = {"titulo" : "Electrónica", "foto1": "electronica1.jpg", "foto2": "electronica2.jpeg", "foto3": "electronica3.png", "producto1": "Computador", "producto2": "iPhone", "producto3": "Televisor"}
     return render(request, 'view/producto.html', data)
 def juguetes(request):
-    data = {"titulo" : "Juguetes"}
+    data = {"titulo" : "Juguetes", "foto1": "juguete1.jpg", "foto2": "juguete2.jpg", "foto3": "juguete3.jpg", "producto1": "Rompecabeza", "producto2": "Peluche", "producto3": "Camion"}
     return render(request, 'view/producto.html', data)
 def ropa(request):
-    data = {"titulo" : "Ropa"}
+    data = {"titulo" : "Ropa", "foto1": "ropa1.jpg", "foto2": "ropa2.jpg", "foto3": "ropa3.jpg", "producto1": "Crop top", "producto2": "Jeans", "producto3": "Poleron"}
     return render(request, 'view/producto.html', data)
