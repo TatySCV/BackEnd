2e7820f2ad6cdd2930d1b12935df4b1bdf16f094
TatySCV <tatiana.campos03@inacapmail.cl>
2023-11-07 11:17:51 -0300

    UP

--- Proyecto_3/Proyecto_3/urls.py
+++ Proyecto_3/Proyecto_3/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Proyecto_5 project.
+URL configuration for Proyecto_3 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestion_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda/', views.busqueda),
+    path('buscar/', views.buscar),
+
 ]
