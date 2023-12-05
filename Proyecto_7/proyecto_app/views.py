e0632981d595029600064d26c6edd88201b7d4ae
TatySCV <tatiana.campos03@inacapmail.cl>
2023-12-05 11:53:45 -0300

    .

--- Proyecto_7/proyecto_app/views.py
+++ Proyecto_7/proyecto_app/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def empleadosview(request):
+    emp = {
+        'id' : 123,
+        'nombre' : 'Juanito',
+        'email' : 'juanito@gmail.com',
+        'sueldo' : '1000000',
+    }
+    return JsonResponse(emp)
